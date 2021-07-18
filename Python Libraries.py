fb3b46ab41644a9dea00e4662943a4e59657f390
Dhruvsinghjadon <75934644+Dhruvsinghjadon@users.noreply.github.com>
2021-07-18 09:37:03 +0530

    Added Libraries

---
+++ Python Libraries.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import seaborn as sb
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import r2_score
+import warnings
+warnings.filterwarnings('always')
+warnings.filterwarnings('ignore')
+import re
+from nltk.corpus import stopwords
+from sklearn.metrics.pairwise import linear_kernel
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
